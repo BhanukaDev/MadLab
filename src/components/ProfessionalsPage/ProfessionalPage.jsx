2cb6dd510e1a4fff1d2f884585e692d9d15aaa94
BhanukaDev <bhanukadassanayake@gmail.com>
2023-06-05 02:36:18 +0530

    Finished Professionals Page

--- src/components/ProfessionalsPage/ProfessionalPage.jsx
+++ src/components/ProfessionalsPage/ProfessionalPage.jsx
@@ -5,32 +5,35 @@ import { CourseButton, TagsBox } from '../CustomTools';
 export const ProfessionalPage = () => {
   return (
     <Box
-      height={'658px'}
       bgcolor='#F7E2A7'
       position={'relative'}
       sx={{
         backgroundImage: 'url("src/assets/Backgrounds/professional.png")',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        px: '84.77px',
-        // display: 'flex',
+        backgroundRepeat: { xs: 'repeat', lg: 'no-repeat' },
+        backgroundSize: { xs: 'auto', lg: 'cover' },
+        backgroundPosition: { xs: 'left', lg: 'center' },
+        px: { xs: '15px', md: '84.77px' },
+        pb: '78px',
       }}
     >
       <Grid container columns={17}>
         <Grid item xs={17} lg={7}>
           <Grid container pl={'2.23px'}>
             <Grid item>
-              <Typography variant='h2' maxWidth={'522px'} mt={'196px'}>
+              <Typography
+                variant='h2'
+                mt={{ xs: '120px', md: '196px' }}
+                sx={{ wordBreak: 'break-word' }}
+              >
                 For Professionals
               </Typography>
-              <Typography maxWidth={'522px'} mt={'22px'} mb={'40px'}>
+              <Typography mt={'22px'} mb={'40px'}>
                 Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                 cupidatat non proident, sunt in culpa qui officia des.
               </Typography>
             </Grid>
-            <Grid item>
+            <Grid item xs={12}>
               <CourseButton title={'View Course'} bgcolor='#BE8C00' />
             </Grid>
           </Grid>
@@ -38,28 +41,29 @@ export const ProfessionalPage = () => {
         <Grid item xs={17} lg={10}>
           <Grid
             container
-            justifyContent={'space-between'}
-            pl={'48px'}
+            justifyContent={{ xs: 'center', lg: 'space-between' }}
+            pl={{ xs: '0px', lg: '48px' }}
             spacing={'29.19px'}
+            columns={{ xs: 14, sm: 12, md: 12, lg: 12 }}
           >
-            <Grid item xs={6}>
+            <Grid item xs={11} sm={5} md={5} lg={6}>
               <CourseCard
                 title='Docker & Kubernetes Course - 12 Sessions in 1'
                 tag={<TagsBox type={'prof'} />}
                 type='medium'
                 media={'src/assets/Courses Images/dockerPrf.png'}
                 sessions={13}
-                mt={'100px'}
+                mt={{ xs: '80px', lg: '100px' }}
               />
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={11} sm={5} md={5} lg={6}>
               <CourseCard
                 title='Docker & Kubernetes Course - 12 Sessions in 1'
                 tag={<TagsBox type={'prof'} />}
                 type='medium'
                 media={'src/assets/Courses Images/dockerPrf.png'}
                 sessions={13}
-                mt={'122px'}
+                mt={{ xs: '0px', sm: '80px', lg: '122px' }}
               />
             </Grid>
           </Grid>
